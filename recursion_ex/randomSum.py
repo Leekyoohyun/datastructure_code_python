76272536b2203887f041c624c0fe5f98aa727cb1
kyoohyunlee <khyhlee@naver.com>
2025-06-15 17:14:23 +0900

    재귀 복습

---
+++ recursion_ex/randomSum.py
@@ -0,0 +1,13 @@
+# 랜덤한 배열 합 
+import random
+
+def arySum(arr, n):
+    if n <= 0:
+        return arr[0]
+    return arySum(arr, n-1) + arr[n]
+
+ary = [random.randint(0,255) for _ in range(random.randint(10,20))]
+
+print(ary)
+
+print("배열합계: ", arySum(ary, len(ary)-1))
